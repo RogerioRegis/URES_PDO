8fee93ef454f3d569257762c3a0efdfd8d86e516
Rogerio Regis <rog_reg@hotmail.com>
2014-07-23 13:59:21 -0400

    [ADJUST] Renome da função para pull
    Signed-off-by: Rogerio Regis <rog_reg@hotmail.com>

--- puxar.php
+++ puxar.php
@@ -1,11 +1,11 @@
 <?php
 
-function puxar($tabela, $idfield, $con, $ident) {
+function pull($tabela, $idfield, $con, $ident) {
     $id = $_GET['id'];
     $query = 'SELECT * FROM '.$tabela.' WHERE '.$idfield.' = :id';
 
     $stmt = $con->prepare($query);
-    $stmt->bindValue(':id', $ident, PDO::PARAM_STR);
+    $stmt->bindValue(':id', $ident, PDO::PARAM_INT);
     $stmt->execute();
     return $stmt->fetch(PDO::FETCH_OBJ);
 }
